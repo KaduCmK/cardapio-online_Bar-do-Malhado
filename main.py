85ddc17d3d027967fa16783dd911e75d0231f21a
Carlos Eduardo <kaduu5050@outlook.com>
2022-04-05 01:15:43 -0300

    Database added
    HTML will now pull data from database and dinamically create products on menu

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def conectar_db():
+    conn = sqlite3.connect('./bdm-produtos.db')
+    conn.row_factory = sqlite3.Row
+    return conn
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    conexao = conectar_db()
+    produtos = conexao.execute('SELECT * FROM comidas').fetchall()
+    conexao.close()
 
-@app.route('/comidas')
-def comidas():
-    return render_template('comidas.html')
-
-@app.route('/bebidas')
-def bebidas():
-    return render_template('bebidas.html')
+    return render_template('index.html', produtos=produtos, format=format)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
